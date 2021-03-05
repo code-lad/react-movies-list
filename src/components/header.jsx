e7a54074be6040b1addf15df48900778d6cfae58
techmanifestsa <techmanifestsa@gmail.com>
2021-03-05 13:19:41 +0200

    bootstrap table render/list moviews

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+          Watch Movies
+        </a>
+      </nav>
+    </div>
+  );
+};
+export default Header;
---
+++ src/components/movies.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+  render() {
+    return (
+      <div className="movies">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th>Control</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button className="btn btn-danger btn-small">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
